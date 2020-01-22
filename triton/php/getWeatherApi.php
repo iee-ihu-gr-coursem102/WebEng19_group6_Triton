053e1c8f8a2e28f888789c5ad8e30e04946b49b6
Vaggelis Stagkos <eyagstag@gmail.com>
2020-01-23 00:00:44 +0200

    Update getWeatherApi.php

--- triton/php/getWeatherApi.php
+++ triton/php/getWeatherApi.php
@@ -1,33 +1,15 @@
 <?php
 
-$url = 'http://api.openweathermap.org/data/2.5/forecast?q=Thessaloniki&appid=626ed06dc10b472392723ab792c57307';
-
-$data = file_get_contents($url);
-
-$array = json_decode($data);
-//echo json_encode($array);
-
-//$metroAreaId=$obj['resultsPage']['results']['location'][0]['metroArea']['id'];
-//echo $metroAreaId;
-
-//$eventData=json_decode($eventData);
-header('Content-Type: application/json');
-//$json_pretty = json_encode(json_decode($data), JSON_PRETTY_PRINT);
-
-$i=0;
-for($i=0;$i<40;$i++){
-    $date = new DateTime();
-    $value=$array->list[$i]->dt;
-    $date=date('d-m-Y', $value);
-    $weather=$array->list[$i]->weather;
-    //echo $date;
-     echo json_encode($weather);
+//$search =$_POST['search'];
+$url="http://api.openweathermap.org/data/2.5/forecast/?q=thessaloniki&appid=626ed06dc10b472392723ab792c57307";
+$json=file_get_contents($url);
+$data=json_decode($json,true);
+$data['city']['name'];
+$temp = array();
+foreach($data['list'] as $day => $value) {
+   $temp[]=array(gmdate("d-m-Y", $value['dt']),$value['weather'][0]['description']);
 }
 
-//$json_pretty_time = json_encode(($array), JSON_PRETTY_PRINT);
-//echo $json_pretty_time;
-
-//echo $json_pretty;
-//echo date("Y-m-d\TH:i:s\Z", $getDate);
-//echo $getDate;
+//$json_pretty = json_encode(json_decode($temp), JSON_PRETTY_PRINT);
+echo json_encode($temp);
 ?>
